628f47a02d441635375db15867cc5f2d9a2b32e3
Omkar-Ajagunde <omkar.ajagunde@ibm.com>
2023-08-05 21:28:40 +0530

    changes

--- generators/nodejs/README.md
+++ generators/nodejs/README.md
@@ -1,4 +1,33 @@
-# How to use GranthAi? (very very simple!)
-1. const GranthAi = require("granthai");
-2. const app = express()
-3. app.use(GranthAi)
+
+
+![](https://github.com/omkarajagunde/granthai/GranthAi.gif)
+
+### How to integrate?
+```js
+const GranthAi = require("granthai")
+const app = express()
+app.use(GranthAi({ 
+    docTitle: "..." // optional
+    key: "..."      // optional
+    baseUrl: "..."  // optional
+}))
+```
+
+### How to access the generated API Documentation?
+When the NodeJs server starts up, GranthAi will add a new public route to your server url at `/api-docs`, you can simply visit this `/api-docs` route to access your generated API Documentation
+
+
+### Is it free to use? 
+
+Absolutely, this tool is completely free to use.
+
+- For the initial documentation of up to 10 APIs, there's no need to sign up or generate a key.
+
+- Once you've documented more than 10 APIs, you can still enjoy free lifetime usage for an unlimited number of APIs. 
+- However, to proceed, please visit [Signup & generate token](https://granthai.com/signup) to complete a simple form and generate a unique token. 
+- This token should then be inserted into the GranthAi init object. 
+- Please rest assured that we don't collect or retain any of your API data or metrics on our servers. 
+- This information is solely utilized for auditing purposes, service enhancement, and gaining insight into user requirements. 
+- If you choose to subscribe to our upcoming features like API metrics & monitoring, live hosting, automated testing, error tracking, and more, we might consider storing such data then.
+
+
--- generators/nodejs/index.js
+++ generators/nodejs/index.js
@@ -7,153 +7,161 @@ const jsonData = {
 		title: "Example API",
 		version: "1.0.0"
 	},
+	servers: [],
 	paths: {}
 };
 
-async function GranthAi(req, res, next) {
-	const send = res.json;
-	let firstpass = false;
-
-	if (req.path.includes("/api-docs")) {
-		return res.send(swaggerTemplate(jsonData));
+async function GranthAi({ docTitle, key, baseUrl }) {
+	jsonData.info.title = docTitle || "API Documentation";
+	if (baseUrl && baseUrl.length > 0) {
+		jsonData.servers.push({ url: baseUrl, description: `${docTitle} server` });
 	}
 
-	res.json = function (body) {
-		if (!firstpass) {
-			firstpass = true;
-			const status = res.statusCode;
-			const paramsLength = Object.keys(req.params).length;
-			let path = req.path;
+	return function (req, res, next) {
+		const send = res.json;
+		let firstpass = false;
 
-			if (paramsLength > 0) {
-				path = path.split("/");
-				const params = req.params;
-				console.log(path);
-				for (let i = 0; i < path.length; i++) {
-					for (obj in params) {
-						if (path[i] === params[obj]) {
-							path[i] = "{" + obj + "}";
-						}
-					}
-				}
-				path = path.join("/");
-			}
-
-			if (!jsonData.paths[path]) {
-				jsonData.paths[path] = {};
-			}
-			const method = req.method.toLowerCase();
-			if (!jsonData.paths[path][method]) {
-				if (method === "get" || method === "delete") {
-					jsonData.paths[path][method] = {
-						summary: "",
-						parameters: [],
-						responses: {}
-					};
-				}
-				if (method === "post" || method === "put" || method === "patch") {
-					jsonData.paths[path][method] = {
-						summary: "",
-						parameters: [],
-						requestBody: { content: {} },
-						responses: {}
-					};
-				}
-			}
-			if (req.params) {
-				const params = req.params;
-				for (const obj in params) {
-					const p = {
-						name: obj,
-						in: "path",
-						required: true,
-						schema: {
-							type: "string"
-						}
-					};
-					if (typeof parseInt(params[obj]) === "number") {
-						p.schema.type = "integer";
-					}
-					let found = false;
-					for (const a of jsonData.paths[path][method].parameters) {
-						if (a.name === p.name && a.in === "path") {
-							found = true;
-						}
-					}
-					if (!found) {
-						jsonData.paths[path][method].parameters.push(p);
-					}
-				}
-			}
-			if (req.query) {
-				const params = req.query;
-				for (const obj in params) {
-					const p = {
-						name: obj,
-						in: "query",
-						schema: {
-							type: "string"
-						},
-						description: ""
-					};
-					if (typeof parseInt(params[obj]) === "number") {
-						p.schema.type = "integer";
-					}
-					let found = false;
-					for (const a of jsonData.paths[path][method].parameters) {
-						if (a.name === p.name && a.in === "query") {
-							found = true;
-							console.log("found");
-						}
-					}
-					if (!found) {
-						jsonData.paths[path][method].parameters.push(p);
-					}
-				}
-			}
-
-			const reqContentType = req.headers["content-type"];
-			const data = req.body;
-
-			if (method === "post" || method === "put" || method === "patch") {
-				jsonData.paths[path][method].requestBody.content[reqContentType] = {
-					schema: generateSchema(data)
-				}; //add schema instead of data later
-				//handle case for true
-				jsonData.paths[path][method].requestBody["required"] = false;
-			}
-
-			if (!jsonData.paths[path][method].responses[status]) {
-				jsonData.paths[path][method].responses[status] = {
-					description: "",
-					content: {}
-				};
-			}
-			let resContentType;
-			if (typeof body === "object") {
-				resContentType = "application/json";
-				jsonData.paths[path][method].responses[status].content[resContentType] = {
-					schema: generateSchema(body)
-				};
-			} else if (typeof body === "string") {
-				resContentType = "text/plain";
-				jsonData.paths[path][method].responses[status].content[resContentType] = {
-					schema: { type: "string" }
-				};
-			} else {
-				resContentType = "string";
-				jsonData.paths[path][method].responses[status].content[resContentType] = {
-					schema: { type: "string" }
-				};
-			}
-
-			// fs.writeFile("./openapi.json", JSON.stringify(jsonData), (err) => {
-			// 	console.log("Written data");
-			// });
+		if (req.path.includes("/api-docs")) {
+			return res.send(swaggerTemplate(jsonData));
 		}
-		send.call(this, body);
+
+		res.json = function (body) {
+			if (!firstpass) {
+				firstpass = true;
+				const status = res.statusCode;
+				const paramsLength = Object.keys(req.params).length;
+				let path = req.originalUrl;
+
+				if (paramsLength > 0) {
+					path = path.split("/");
+					const params = req.params;
+					console.log(path);
+					for (let i = 0; i < path.length; i++) {
+						for (obj in params) {
+							if (path[i] === params[obj]) {
+								path[i] = "{" + obj + "}";
+							}
+						}
+					}
+					path = path.join("/");
+				}
+
+				if (!jsonData.paths[path]) {
+					jsonData.paths[path] = {};
+				}
+				const method = req.method.toLowerCase();
+				if (!jsonData.paths[path][method]) {
+					if (method === "get" || method === "delete") {
+						jsonData.paths[path][method] = {
+							summary: "",
+							parameters: [],
+							responses: {}
+						};
+					}
+					if (method === "post" || method === "put" || method === "patch") {
+						jsonData.paths[path][method] = {
+							summary: "",
+							parameters: [],
+							requestBody: { content: {} },
+							responses: {}
+						};
+					}
+				}
+				if (req.params) {
+					const params = req.params;
+					for (const obj in params) {
+						const p = {
+							name: obj,
+							in: "path",
+							required: true,
+							schema: {
+								type: "string"
+							}
+						};
+						if (typeof parseInt(params[obj]) === "number") {
+							p.schema.type = "integer";
+						}
+						let found = false;
+						for (const a of jsonData.paths[path][method].parameters) {
+							if (a.name === p.name && a.in === "path") {
+								found = true;
+							}
+						}
+						if (!found) {
+							jsonData.paths[path][method].parameters.push(p);
+						}
+					}
+				}
+				if (req.query) {
+					const params = req.query;
+					for (const obj in params) {
+						const p = {
+							name: obj,
+							in: "query",
+							schema: {
+								type: "string"
+							},
+							description: ""
+						};
+						if (typeof parseInt(params[obj]) === "number") {
+							p.schema.type = "integer";
+						}
+						let found = false;
+						for (const a of jsonData.paths[path][method].parameters) {
+							if (a.name === p.name && a.in === "query") {
+								found = true;
+								console.log("found");
+							}
+						}
+						if (!found) {
+							jsonData.paths[path][method].parameters.push(p);
+						}
+					}
+				}
+
+				const reqContentType = req.headers["content-type"];
+				const data = req.body;
+
+				if (method === "post" || method === "put" || method === "patch") {
+					jsonData.paths[path][method].requestBody.content[reqContentType] = {
+						schema: generateSchema(data)
+					}; //add schema instead of data later
+					//handle case for true
+					jsonData.paths[path][method].requestBody["required"] = false;
+				}
+
+				if (!jsonData.paths[path][method].responses[status]) {
+					jsonData.paths[path][method].responses[status] = {
+						description: "",
+						content: {}
+					};
+				}
+				let resContentType;
+				if (typeof body === "object") {
+					resContentType = "application/json";
+					jsonData.paths[path][method].responses[status].content[resContentType] = {
+						schema: generateSchema(body)
+					};
+				} else if (typeof body === "string") {
+					resContentType = "text/plain";
+					jsonData.paths[path][method].responses[status].content[resContentType] = {
+						schema: { type: "string" }
+					};
+				} else {
+					resContentType = "string";
+					jsonData.paths[path][method].responses[status].content[resContentType] = {
+						schema: { type: "string" }
+					};
+				}
+
+				// fs.writeFile("./openapi.json", JSON.stringify(jsonData), (err) => {
+				// 	console.log("Written data");
+				// });
+			}
+			send.call(this, body);
+		};
+		next();
 	};
-	next();
 }
 
 module.exports = GranthAi;
