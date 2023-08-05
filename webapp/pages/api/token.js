c22dbe8661041cd98ce5ff6b60953d9ac75a3d2a
Omkar-Ajagunde <omkar.ajagunde@ibm.com>
2023-08-05 22:53:39 +0530

    changes

--- webapp/pages/api/token.js
+++ webapp/pages/api/token.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
 	if (req.method === "POST") {
-		console.log("LOGG -- ", req.body);
+		console.log("Incoming webhook from Tally -- ", JSON.stringify(req.body));
 		res.status(200).json({ name: "John Doe" });
 	}
 }
