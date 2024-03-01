6cb748d704f9f45d6231be1637012367df70b1f2
Omkar-Ajagunde <omkar.ajagunde@ibm.com>
2024-03-01 23:12:05 +0530

    updated code

--- webapp/pages/api/token.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-	if (req.method === "POST") {
-		console.log("Incoming webhook from Tally -- ", JSON.stringify(req.body));
-		res.status(200).json({ name: "John Doe" });
-	}
-}
